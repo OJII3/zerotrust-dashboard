58d5d42c4cdf811daf07228067e4abbbdb187b36
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-07-31 20:37:29 +0900

    feat: add vite config

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()],
+  root: "./src/client/",
+  build: { outDir: "../../dist" }
+});
